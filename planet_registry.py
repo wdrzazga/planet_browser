9b82a21f06b6599cc99b154f6218955c4890aeca
wdrzazga <popownica@gmail.com>
2024-08-16 10:40:44 +0200

    Added every planet

--- planet_registry.py
+++ planet_registry.py
@@ -8,4 +8,8 @@ class PlanetRegistry:
         venus = Planet('Wenus', 'Venus is the hottest planet.', 'resources/images/Venus.jpg')
         earth = Planet('Earth', 'You are probably here.', 'resources/images/Earth.jpg')
         mars = Planet('Mars', 'Robots took over Mars', 'resources/images/Mars.jpg')
-        self.planets = [mercury, venus, earth, mars]
+        jupiter = Planet('Jupiter', 'Jupiter is the biggest planet', 'resources/images/Jupiter.jpg')
+        saturn = Planet('Saturn', 'Saturn has the biggest rings', 'resources/images/Saturn.jpg')
+        uranus = Planet('Uranus', 'Uranus is rotated by approximately 89°', 'resources/images/Uranus.jpg')
+        neptune = Planet('Neptune', 'Neptune is the coldest planet', 'resources/images/Neptune.jpg')
+        self.planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
